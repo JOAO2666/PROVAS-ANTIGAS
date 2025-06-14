012230c3db6b462db33630b482c527fc897a4c93
João Emanuel <108814202+JOAO2666@users.noreply.github.com>
2025-06-14 17:06:56 -0300

    Feat: Reconfigura o deploy para o GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    basePath: '/PROVAS-ANTIGAS',
+    images: {
+        unoptimized: true,
+    },
+};
 
 module.exports = nextConfig; 
